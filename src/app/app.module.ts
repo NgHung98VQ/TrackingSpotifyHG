ef9f99543a2b5e151e0a070356d1e085c78cc496
NgHung98VQ <sharkilybk@gmail.com>
2022-11-09 14:22:57 +0700

    [HungIT] TrackingSpotify

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TrackingService } from './service/tracking.service';
+import { NgProgressModule } from 'ngx-progressbar';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,
+    CommonModule,
+    NgProgressModule
+  ],
+  providers: [TrackingService,{provide: "BASE_API_URL", useValue: "http://192.168.10.153:3129/api/TrackingSpotify/" }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
